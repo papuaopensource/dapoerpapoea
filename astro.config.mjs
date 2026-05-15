ffa3925b98724603924a3fd8c3efba266df68238
Antheiz <antheiz.dev@gmail.com>
2026-05-15 11:52:08 +0900

    feat: add sitemap and robots.txt closes #4

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import tailwind from '@astrojs/tailwind';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://dapoerpapoea.pages.dev",
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: 'server',
   adapter: cloudflare()
 });
